25dd98b6e4f00b14c2da6145c8a531a41e119b7b
Diego Cordova <diego.cordova.do.prado@gmail.com>
2021-11-24 19:30:54 -0400

    LOGS VOLUME

--- ms-student/src/main/java/arquitectura/software/msstudent/api/StudentController.java
+++ ms-student/src/main/java/arquitectura/software/msstudent/api/StudentController.java
@@ -2,6 +2,8 @@ package arquitectura.software.msstudent.api;
 
 import arquitectura.software.msstudent.entity.Student;
 import arquitectura.software.msstudent.repository.StudentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,12 +13,18 @@ import java.util.Optional;
 @RestController
 @RequestMapping("v1/api/student")
 public class StudentController {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(StudentController.class);
+
     @Autowired
     private StudentRepository studentRepository;
 
     @RequestMapping(path = "/test",
             method = RequestMethod.GET)
     public String testCustomer(){
+        LOGGER.error("Error Logger");
+        LOGGER.warn("WARN LOGGER");
+        LOGGER.info("Probandi endpoint ms-student");
         return "MS - student";
     }
 
